527c92339118721bd9c4301a89b9c82c9244e43c
Conor Schaefer <conor.schaefer@gmail.com>
2017-02-19 19:34:31 -0800

    Creates tests in molecule config

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,6 +4,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
+def test_mocp_package(Package):
+    p = Package('moc')
+    assert p.is_installed
+
+
 def test_hosts_file(File):
     f = File('/etc/hosts')
 
